b099d02d272b9d64ac57c5678f2aebfd1208cf61
Brian Nelson <brian@treegecko.com>
2016-07-02 09:00:35 -0600

    Extended out location constants

--- src/Constants/LocationConstants.cs
+++ src/Constants/LocationConstants.cs
@@ -7,9 +7,20 @@ namespace ATMobile.Constants
     {
         //TODO - Move to database and should sync down from server
 
-        public static List<string> USA_States = new List<string> () { "CA", "CO" };
+        public static List<string> USA_States = new List<string> () {
+            "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+            "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+            "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+            "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+            "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC"
+        };
 
-        public static List<string> Countries = new List<string> () { "USA" };
+        public static List<string> CA_Provinces = new List<string> () {
+            "AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE",
+            "QC", "SK", "YT"
+        };
+
+        public static List<string> Countries = new List<string> () { "USA", "CAN" };
     }
 }
 
